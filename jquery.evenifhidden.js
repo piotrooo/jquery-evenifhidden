52d87fcdbcbc34174dd55022fed0774c9f0840e4
Piotr Olaszewski <piotroo89@gmail.com>
2013-12-05 15:15:55 +0100

    Create jquery.evenifhidden.js

---
+++ jquery.evenifhidden.js
@@ -0,0 +1,37 @@
+/*!
+ * jQuery evenIfHidden
+ * https://github.com/piotrooo/jquery-evenifhidden
+ *
+ * Copyright 2013 Piotr Olaszewski
+ * Released under the MIT license
+ */
+(function ($) {
+    $.fn.evenIfHidden = function (callback) {
+        return this.each(function () {
+            var _this = $(this);
+            var currentStyles = [];
+
+            var hiddenElements = _this.parents().andSelf().filter(':hidden');
+
+            if (!hiddenElements.length) {
+                callback(_this);
+                return;
+            }
+
+            hiddenElements.each(function () {
+                var style = $(this).attr('style');
+                style = typeof style == 'undefined' ? '' : style;
+                currentStyles.push(style);
+                $(this).attr('style', style + ' display: block !important;');
+            });
+
+            hiddenElements.eq(0).css('left', -10000);
+
+            callback(_this);
+
+            hiddenElements.each(function () {
+                $(this).attr('style', currentStyles.shift());
+            });
+        });
+    };
+}(jQuery));
